d0aa5b90cb5eea5b936869a13d9dac64989b84d2
Kyu Hyong Park <kjp5774@psu.edu>
2022-08-05 14:13:10 -0400

    Separate update mask tests

--- cubewalkers/tests/test_update_mask.py
+++ cubewalkers/tests/test_update_mask.py
@@ -4,21 +4,19 @@ import cubewalkers as cw
 n = 10
 w = 15
 
-def test_update_mask():
-    '''
-    Tests few update masks.
-    '''
-
+def test_update_asynchronous():
     asynch = cw.update_schemes.asynchronous(None, n, w, None)
     updates_per_walker = cp.sum(asynch,axis=0)
     for update in updates_per_walker:
         assert update == 1, 'One node should be updated for each walker'
 
+def test_update_synchronous():
     synch = cw.update_schemes.synchronous(None, n, w, None)
     min = cp.min(synch,axis=0)
     for i in range(w):
         assert  min[i] == 1, 'All nodes must be updated for each walker'
 
+def test_update_synchronous_PBN():
     synch_pbn = cw.update_schemes.synchronous_PBN(None, n, w, None)
     max = cp.max(synch_pbn,axis=0)
     min = cp.min(synch_pbn,axis=0)
@@ -26,6 +24,7 @@ def test_update_mask():
         assert  max[i] != min[i], 'All nodes must use the different value for each walker'
         assert  min[i] != 0, 'All nodes must be updated for each walker'
 
+def test_update_synchronous_PBN_dependent():
     synch_pbn_dep = cw.update_schemes.synchronous_PBN_dependent(None, n, w, None)
     max = cp.max(synch_pbn_dep,axis=0)
     min = cp.min(synch_pbn_dep,axis=0)
