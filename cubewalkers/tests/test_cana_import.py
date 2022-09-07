63aed1b2b791c7748ccef81c203d32530c1bb099
FXCosta <fcosta@example.com>
2022-09-07 09:34:57 -0400

    Added test to cana_import

---
+++ cubewalkers/tests/test_cana_import.py
@@ -0,0 +1,20 @@
+# Cubewalkers import
+from cubewalkers import parser as cwp 
+
+# CANA import
+import cana.boolean_network as cana_bn
+
+
+## Feed-Forwward motif with NOT+AND
+logic = {0: {'name': '3A', 'in':[], 'out':[0]},
+         1: {'name': 'B+', 'in':[0], 'out':[1, 0]},
+         2: {'name': '(C)', 'in':[0, 1], 'out':[0, 0, 0, 1]}}
+
+cw_rules = "number_3A* = 0\nB_plus_* = !(number_3A)\n_C_* = number_3A&B_plus_"
+
+def test_cana_import():
+    
+    BN = cana_bn.BooleanNetwork.from_dict(logic)
+    cana_rules = cwp.network_rules_from_cana(BN)
+    
+    assert(cana_rules == cw_rules)
