53be10959c44094ec88ade6c2968092e8aad549e
Jordan Rozum <jcrozum@gmail.com>
2022-09-07 16:16:25 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="cubewalkers",
+    version='1.0.0',
+    author="Jordan Rozum",
+    author_email="jrozum@binghamton.edu",
+    description="Python package for simulating Boolean networks on the GPU",
+    url="https://github.com/jcrozum/cubewalkers",
+    license='MIT',
+    python_requires='>=3.8',
+    packages=['cubewalkers']
+    )
