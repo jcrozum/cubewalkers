37ef91b9ada037ac373a478e33d56f618e4dc95c
Jordan Rozum <jcrozum@gmail.com>
2022-10-27 10:00:51 -0400

    Added test for coherence wrapper function

--- cubewalkers/tests/test_coherence.py
+++ cubewalkers/tests/test_coherence.py
@@ -11,3 +11,11 @@ def test_source_coherence_zero():
                        model_name='source_coherence_zero')
     c = mymodel.source_coherence('A',T_sample=100)
     assert c == 0.0
+    
+def test_coherence_zero():
+    mymodel = cw.Model(rules_zero,
+                       n_time_steps=1000,
+                       n_walkers=1000,
+                       model_name='source_coherence_zero')
+    c = mymodel.coherence(T_sample=100)
+    assert c == 0.0
