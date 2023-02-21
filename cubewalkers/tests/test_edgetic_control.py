aef6761b12d89acb4c4e8130f99446bb0a1c3ae0
jcrozum <jcrozum@gmail.com>
2023-02-21 14:29:01 -0500

    Fixed error in edge control

--- cubewalkers/tests/test_edgetic_control.py
+++ cubewalkers/tests/test_edgetic_control.py
@@ -15,11 +15,10 @@ experiment_edge_string = '''
 A-->B,0,inf,1
 '''
 
-# In case of contradiction, higher override should win
-experiment_edge_string_conflict = '''
-A-->B,0,inf,1
+experiment_edge_string_alt = '''
 A-->B,0,inf,0
 '''
+
 
 def test_node_control():
     experiment_node = cw.Experiment(experiment_node_string)
@@ -32,24 +31,21 @@ def test_node_control():
     assert(cp.all(node_control_model.trajectories[-1]==cp.array([[True],[True],[True]])))
     
 def test_edge_control():
+
     experiment_edge = cw.Experiment(experiment_edge_string)
     edge_control_model = cw.Model(rules,experiment=experiment_edge,model_name='edge_control')
+    experiment_edge_alt = cw.Experiment(experiment_edge_string_alt)
+    edge_control_model_alt = cw.Model(rules,experiment=experiment_edge_alt,model_name='edge_control_alt')
     
     edge_control_model.n_time_steps=5
-    edge_control_model.n_walkers=1
+    edge_control_model.n_walkers=100
+    edge_control_model_alt.n_time_steps=5
+    edge_control_model_alt.n_walkers=100
     
     edge_control_model.simulate_ensemble(maskfunction=cw.update_schemes.synchronous)
+    edge_control_model_alt.simulate_ensemble(maskfunction=cw.update_schemes.synchronous)
+
+    print(edge_control_model_alt.trajectories)
+
     assert(cp.all(edge_control_model.trajectories[-1]==cp.array([[False],[True],[False]])))
-    
-def test_edge_control_conflict():
-    experiment_edge_conflict = cw.Experiment(experiment_edge_string_conflict)
-    edge_control_model_conflict = cw.Model(rules,experiment=experiment_edge_conflict,model_name='edge_control_conflict')
-    
-    edge_control_model_conflict.n_time_steps=5
-    edge_control_model_conflict.n_walkers=1
-    
-    edge_control_model_conflict.simulate_ensemble(maskfunction=cw.update_schemes.synchronous)
-    assert(cp.all(edge_control_model_conflict.trajectories[-1]==cp.array([[False],[True],[False]])))
-
-    
-
+    assert(cp.all(edge_control_model_alt.trajectories[-1]==cp.array([[False],[False],[False]])))
