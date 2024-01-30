b5eb0e69d4f3d100abba18e3fbf7c1a45468d839
jcrozum <jcrozum@gmail.com>
2024-01-30 14:14:24 -0500

    increment version

--- cubewalkers/__init__.py
+++ cubewalkers/__init__.py
@@ -5,3 +5,5 @@ from cubewalkers import simulation as simulation  # noqa: F401
 from cubewalkers import update_schemes as update_schemes  # noqa: F401
 from cubewalkers.experiment import Experiment as Experiment  # noqa: F401
 from cubewalkers.model import Model as Model  # noqa: F401
+
+__version__ = "1.4.0"
