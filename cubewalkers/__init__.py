c66c79b8b4a2eae8b839515d279af347795e37f0
jcrozum <jcrozum@gmail.com>
2024-01-30 13:10:06 -0500

    requoted __all__ symbols

--- cubewalkers/__init__.py
+++ cubewalkers/__init__.py
@@ -10,11 +10,11 @@ from cubewalkers.experiment import Experiment as Experiment
 from cubewalkers.model import Model as Model
 
 __all__ = [
-    conversions,
-    initial_conditions,
-    parser,
-    simulation,
-    update_schemes,
-    Experiment,
-    Model,
+    "conversions",
+    "initial_conditions",
+    "parser",
+    "simulation",
+    "update_schemes",
+    "Experiment",
+    "Model",
 ]
