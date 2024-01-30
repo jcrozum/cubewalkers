19b865997eea4e93de4b1ac9da4c70832144e344
jcrozum <jcrozum@gmail.com>
2024-01-30 14:07:18 -0500

    change import structure

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,7 @@
-# type: ignore
-from cubewalkers import (
-    conversions,
-    initial_conditions,
-    parser,
-    simulation,
-    update_schemes,
-)
-from cubewalkers.experiment import Experiment as Experiment
-from cubewalkers.model import Model as Model
-
-__all__ = [
-    conversions,
-    initial_conditions,
-    parser,
-    simulation,
-    update_schemes,
-    Experiment,
-    Model,
-]
+from cubewalkers import conversions as conversions  # noqa: F401
+from cubewalkers import initial_conditions as initial_conditions  # noqa: F401
+from cubewalkers import parser as parser  # noqa: F401
+from cubewalkers import simulation as simulation  # noqa: F401
+from cubewalkers import update_schemes as update_schemes  # noqa: F401
+from cubewalkers.experiment import Experiment as Experiment  # noqa: F401
+from cubewalkers.model import Model as Model  # noqa: F401
